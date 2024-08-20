5e6903710f5ec97b8492d251cf63d0ae1535fd48
akoka <akoka@altimetrik.com>
2024-08-20 15:03:48 +0530

    commit 7

--- src/test/java/serenitytest/LoginTest.java
+++ src/test/java/serenitytest/LoginTest.java
@@ -1,57 +1,43 @@
 package serenitytest;
+
 import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.annotations.Title;
+import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.*;
 
 
-
 @RunWith(SerenityRunner.class)
-public class LoginTest extends PageObject {
+public class LoginTest extends PageObject{
 
     @Managed(driver="chrome")
     WebDriver driver;
-//    static Actions action;
-//    static Alert alert;
-//    static JavascriptExecutor js;
 
+    JavascriptExecutor js;
+
+    @FindBy(xpath="//*[@id='permanentAddress']")
+    WebElement PermanentAddr;
+
+
+    @Title("Executing a login Test")
     @Test
     public void doLogin() throws InterruptedException {
         open();
-      //  driver.get("https://demoqa.com");
+        js=(JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,300)");
+        $("//*[@class=\"category-cards\"]//following::div[@class=\"card-body\"]//h5[contains(text(),\"Elements\")]").click();
+        getDriver().findElement((By.xpath("//*[@class=\"accordion\"]//div[@class=\"element-group\"]//following::span[@class=\"text\" and contains(text(),\"Text Box\")]"))).click();
+        find(By.xpath("//*[@id=\"userName\"]")).sendKeys("sita");
+        find(By.xpath("//*[@id=\"userEmail\"]")).sendKeys("sita@gmail.com");
+        typeInto($("//*[@id=\"currentAddress\"]"),"sita@gmail.com");
+        typeInto($("//*[@id=\"permanentAddress\"]"), "sita@gmail.com");
+        find(By.xpath("//*[@id=\"submit\"]")).submit();
 
-//       js=(JavascriptExecutor) driver;
-//       js.executeScript("window.scrollBy(0,300)");
-//
-//
-//       WebElement ele= driver.findElement(By.xpath("//*[@class=\"category-cards\"]//following::div[@class=\"card-body\"]//h5[contains(text(),\"Elements\")]"));
-//       ele.click();
-//       WebElement textbox=driver.findElement(By.xpath("//*[@class=\"accordion\"]//div[@class=\"element-group\"]//following::span[@class=\"text\" and contains(text(),\"Text Box\")]"));
-//       textbox.click();
-//
-//
-//       String sText=js.executeScript("return document.title;").toString();
-//       WebElement element= driver.findElement(By.xpath("//*[contains(text(),\"Text Box\")]"));
-//       String sText1=js.executeScript("return document.title;").toString();
-//       System.out.println(sText1);
-//       String sText2=js.executeScript("return document.domain;").toString();
-//       System.out.println(sText2);
-//       String sText3=js.executeScript("return document.URL;").toString();
-//       System.out.println(sText3);
-//
-//
-//       js.executeScript("arguments[0].scrollIntoView;",element);
-//       js.executeScript("location.reload()");
-//       js.executeScript("document.getElementById('userName').value='sita';");
-//       js.executeScript("document.getElementById('userEmail').value='sita@gmail.com';");
-//       js.executeScript("document.getElementById('currentAddress').value='sita@gmail.com';");
-//       js.executeScript("document.getElementById('permanentAddress').value='sita@gmail.com';");
-//       js.executeScript("document.getElementById('submit').click();");
-//       List<WebElement> ele3=driver.findElements(By.xpath("//*[@id=\"output\"]//p"));
-//
-//
+//       Set<WebElementFacade> ele3 = find(By.xpath("//*[@id=\"output\"]//p"));
 //       if(ele3.size()!=0){
 //           System.out.println(ele3.size()+"Element found as tag name as input \n");
 //           for(WebElement inputele:ele3)
@@ -59,10 +45,8 @@ public class LoginTest extends PageObject {
 //               System.out.println(inputele.getText());
 //           }
 //       }
-//
-//
-//       //  System.out.println(name.toString());
-//       Thread.sleep(1000);
+
+
     }
 
 }
