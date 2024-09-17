62382391c010193036b3d4225e52169add3e19ac
akoka <akoka@altimetrik.com>
2024-09-17 18:11:03 +0530

    Seleniumdemo5

---
+++ src/main/java/org/example/javaDemo/Arrays/ReverseElement.java
@@ -0,0 +1,19 @@
+package org.example.javaDemo.Arrays;
+
+public class ReverseElement
+{
+    public static void main(String[] args) {
+        int[] arr=new int[]{1,2,3,4,5};
+        System.out.println("Original Array");
+
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Array in reverse order:");
+
+        for(int i= args.length-1;i>=0;i--){
+            System.out.println(arr[i]+"");
+        }
+    }
+}
