204776ff43a530e10fb81751a686db1d65f5603c
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2025-01-29 20:06:18 +0530

    commit 29-1-2025 -08

---
+++ src/test/java/cucumberBddTest/uiStepDef/AlertsStepdefs.java
@@ -0,0 +1,47 @@
+package cucumberBddTest.uiStepDef;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AlertsStepdefs {
+
+
+
+    @Given("I am in Alert landing page")
+    public void iAmInAlertLandingPage() {
+    }
+
+    @When("I click on First Button")
+    public void iClickOnFirstButton() {
+
+    }
+
+    @Then("I am able to see alert Pop Up")
+    public void iAmAbleToSeeAlertPopUp() {
+    }
+
+    @When("I click on second Button")
+    public void iClickOnSecondButton() {
+    }
+
+    @Then("I am able to see alert Pop Up in {int} min")
+    public void iAmAbleToSeeAlertPopUpInMin(int arg0) {
+    }
+
+    @When("I click on third Button")
+    public void iClickOnThirdButton() {
+    }
+
+    @Then("I am able to see alert confirm box")
+    public void iAmAbleToSeeAlertConfirmBox() {
+    }
+
+    @When("I click on fourth Button")
+    public void iClickOnFourthButton() {
+    }
+
+    @Then("I am able to see alert  prompt box")
+    public void iAmAbleToSeeAlertPromptBox() {
+    }
+}
---
+++ src/test/java/cucumberBddTest/uiStepDef/BrowserStepDefs.java
@@ -0,0 +1,35 @@
+package cucumberBddTest.uiStepDef;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BrowserStepDefs {
+    @Given("I am in Browser landing page")
+    public void iAmInBrowserLandingPage() {
+    }
+
+    @When("I click on New Tab Button")
+    public void iClickOnNewTabButton() {
+    }
+
+    @Then("I am able to launch new Tab")
+    public void iAmAbleToLaunchNewTab() {
+    }
+
+    @When("I click on Window Tab Button")
+    public void iClickOnWindowTabButton() {
+    }
+
+    @Then("I am able to launch new Window")
+    public void iAmAbleToLaunchNewWindow() {
+    }
+
+    @When("I click on New Window Message Button")
+    public void iClickOnNewWindowMessageButton() {
+    }
+
+    @Then("I am able to launch new Window Message Window")
+    public void iAmAbleToLaunchNewWindowMessageWindow() {
+    }
+}
