a73a422042894617f86e67854f38fdfcd808758f
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2025-01-30 15:01:02 +0530

    commit 30-1-2025 -03

--- src/test/java/cucumberBddTest/apiStepDef/APITestSteps.java
+++ src/test/java/cucumberBddTest/apiStepDef/APITestSteps.java
@@ -60,4 +60,12 @@ public class APITestSteps {
 
 
 
+    @When("I send request for books avaliable")
+    public void iSendRequestForBooksAvaliable() {
+
+    }
+
+    @Then("I am able to successfully verify response with List of Book and details as {string} ,{string} ,{string}, {string}, {string}, {string}, {string}")
+    public void iAmAbleToSuccessfullyVerifyResponseWithListOfBookAndDetailsAs(String arg0, String arg1, String arg2, String arg3, String arg4, String arg5, String arg6, String arg7, String arg8, String arg9, String arg10, String arg11, String arg12, String arg13) {
+    }
 }
