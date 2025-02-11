332376e19316ee355477428ee7bdbe6f9b1e86fa
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2025-02-11 13:09:12 +0530

    commit 11-2-2025 -07

--- src/test/java/seleniumGridTest/RemoteDriverBaseClass.java
+++ src/test/java/seleniumGridTest/RemoteDriverBaseClass.java
@@ -34,8 +34,7 @@ public class RemoteDriverBaseClass {
         }
         if (browser.equalsIgnoreCase("chrome")) {
             ChromeOptions chromeOptions = new ChromeOptions();
-            // driver = (new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions));
-            driver = (new RemoteWebDriver(new URL("http://"+getLocalHostAddress()+":4444"), chromeOptions));
+             driver = (new RemoteWebDriver(new URL("http://"+getLocalHostAddress()+":4444"), chromeOptions));
         }
         else if (browser.equalsIgnoreCase("firefox")) {
             FirefoxOptions firefoxOptions = new FirefoxOptions();
@@ -108,7 +107,8 @@ public class RemoteDriverBaseClass {
         return process;
     }
 
-    //Step1: Start the Event Bus, Event Bus helps in internal communication between different grid components.
+    //Step1: Start the Event Bus,
+    // Event Bus helps in internal communication between different grid components.
     private static Process launchEventBusProcess(String jarPath) throws IOException {
         ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", jarPath, "event-bus", "--publish-events", "tcp://"+getLocalHostAddress()+":4442", "--subscribe-events", "tcp://"+getLocalHostAddress()+":4443 --port 5557");
         processBuilder.redirectErrorStream(true);
@@ -117,10 +117,10 @@ public class RemoteDriverBaseClass {
         return process;
     }
 
-    //Step 2: Start the New Session Queue,Start the New Session Queue by adding the new session requests to a queue.
+    //Step 2: Start the New Session Queue,
+    // Start the New Session Queue by adding the new session requests to a queue.
     //The Distributor queries it.
     private static Process launchSessionQueueProcess(String jarPath) throws IOException {
-
         ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", jarPath, "sessionqueue", "--port 5559");
         processBuilder.redirectErrorStream(true);
         Process process = processBuilder.start();
@@ -128,7 +128,8 @@ public class RemoteDriverBaseClass {
         return process;
     }
 
-    //Step3: Start the Session Map,Start the Session Map next, which will interact with the Event Bus and
+    //Step3: Start the Session Map,Start the Session Map next,
+    // which will interact with the Event Bus and
     //map session IDs to the Node where the session is running.
     private static Process launchSessionMappingProcess(String jarPath) throws IOException {
      ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", jarPath, "sessions", "--publish-events", "tcp://"+getLocalHostAddress()+":4442", "--subscribe-events", "tcp://"+getLocalHostAddress()+":4443","--port 5556");
@@ -138,8 +139,10 @@ public class RemoteDriverBaseClass {
         return process;
     }
 
-    //Step 4: start the Distributor,which queries the New Session Queue for checking new session requests.
-    //When finding the matching capabilities, it assigns a Node to the New Session request.
+    //Step 4: start the Distributor,
+    // which queries the New Session Queue for checking new session requests.
+    //When finding the matching capabilities,
+    // it assigns a Node to the New Session request.
 private static Process launchDistributorService(String jarPath) throws IOException {
    ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", jarPath, "distributor", "--publish-events", "tcp://"+getLocalHostAddress()+":4442", "--subscribe-events", "tcp://"+getLocalHostAddress()+":4443",  "--sessions", "http://"+getLocalHostAddress()+":5556", "--sessionqueue", "http://"+getLocalHostAddress()+":5559", "--port 5553", "--bind-bus false");
     processBuilder.redirectErrorStream(true);
@@ -148,7 +151,9 @@ private static Process launchDistributorService(String jarPath) throws IOExcepti
     return process;
 }
 
-//Step 5: Start the Router,The next step is to start the Router, which will direct new session requests to the queue and route requests for active sessions to the Node handling that session.
+//Step 5: Start the Router,The next step is to start the Router,
+// which will direct new session requests to the queue and
+// route requests for active sessions to the Node handling that session.
 private static Process launchRouterService(String jarPath) throws IOException {
     ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", jarPath, "router --sessions", "http://"+getLocalHostAddress()+":5556", "--distributor", "http://"+getLocalHostAddress()+":5553", "--sessionqueue", "http://"+getLocalHostAddress()+":5559", "--port 4444");
     processBuilder.redirectErrorStream(true);
@@ -157,8 +162,10 @@ private static Process launchRouterService(String jarPath) throws IOException {
     return process;
 }
 
-//Step 6:  Start the Nodes,Start the Node to launch the browser sessions, which will eventually help run our automated tests.
-//The following command will add one Node with four Chrome, Firefox, and Edge browser sessions. It will also spin one session of IE browser by default.
+//Step 6:  Start the Nodes,Start the Node to launch the browser sessions,
+//which will eventually help run our automated tests.
+//The following command will add one Node with four Chrome, Firefox, and Edge browser sessions.
+// It will also spin one session of IE browser by default.
 private static Process launchBrowserNode(String jarPath) throws IOException {
     ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", jarPath, "node", "--publish-events", "tcp://"+getLocalHostAddress()+":4442", "--subscribe-events", "tcp://"+getLocalHostAddress()+":4443");
     processBuilder.redirectErrorStream(true);
@@ -168,30 +175,37 @@ private static Process launchBrowserNode(String jarPath) throws IOException {
 }
 
 public static void startDistributedServer(String jarPath) throws IOException, InterruptedException {
+
     Process eventBus= launchEventBusProcess(jarPath);
     logServerOutput(eventBus);
     int exitCode3 = eventBus.waitFor();
     System.out.println("Process exited with code: " + exitCode3);
-    Process sessionQueue= launchSessionQueueProcess(jarPath);
-    logServerOutput(sessionQueue);
-    int exitCode4 = sessionQueue.waitFor();
-    System.out.println("Process exited with code: " + exitCode4);
-    Process SessionMap= launchSessionMappingProcess(jarPath);
-    logServerOutput(SessionMap);
-    int exitCode5 = SessionMap.waitFor();
-    System.out.println("Process exited with code: " + exitCode5);
-    Process distributorServer= launchDistributorService(jarPath);
-    logServerOutput(distributorServer);
-    int exitCode6 = distributorServer.waitFor();
-    System.out.println("Process exited with code: " + exitCode6);
+
     Process routerServer= launchRouterService(jarPath);
     logServerOutput(routerServer);
     int exitCode7 = routerServer.waitFor();
     System.out.println("Process exited with code: " + exitCode7);
+
+    Process sessionQueue= launchSessionQueueProcess(jarPath);
+    logServerOutput(sessionQueue);
+    int exitCode4 = sessionQueue.waitFor();
+    System.out.println("Process exited with code: " + exitCode4);
+
+    Process distributorServer= launchDistributorService(jarPath);
+    logServerOutput(distributorServer);
+    int exitCode6 = distributorServer.waitFor();
+    System.out.println("Process exited with code: " + exitCode6);
+
     Process Node12= launchBrowserNode(jarPath);
     logServerOutput(Node12);
     int exitCode8 = Node12.waitFor();
     System.out.println("Process exited with code: " + exitCode8);
+
+    Process SessionMap= launchSessionMappingProcess(jarPath);
+    logServerOutput(SessionMap);
+    int exitCode5 = SessionMap.waitFor();
+    System.out.println("Process exited with code: " + exitCode5);
+
 }
 
 
