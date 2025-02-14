d41f60f0a880529109b8af1573ef90f85ba4e9f1
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2025-02-14 12:05:37 +0530

    commit 14-2-2025 -01

--- src/test/java/seleniumGridTest/StandaloneTest.java
+++ src/test/java/seleniumGridTest/StandaloneTest.java
@@ -1,10 +1,8 @@
 package seleniumGridTest;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
-
 import java.io.IOException;
 import java.util.List;
 
