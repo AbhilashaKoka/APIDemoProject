8f39f812a8a83c8ff24a22c5e857a2a4a573095d
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2025-03-12 12:20:03 +0530

    commit 12-03-2025 -01

---
+++ src/test/java/seleniumperfTest/BaseproxySetUp.java
@@ -0,0 +1,4 @@
+package seleniumperfTest;
+
+public class BaseproxySetUp {
+}
--- src/test/java/seleniumperfTest/SeleniumBrowserMobWithProxy.java
+++ src/test/java/seleniumperfTest/SeleniumBrowserMobWithProxy.java
@@ -10,55 +10,43 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class SeleniumBrowserMobWithProxy {
+public class SeleniumBrowserMobWithProxy extends BaseproxySetUp{
 
     static WebDriver driver;
-    static Actions action;
-    static JavascriptExecutor js ;
+     static JavascriptExecutor js ;
 
     public static void main(String[] args) throws IOException, InterruptedException {
-
         // Step 1: Start BrowserMob Proxy
         BrowserMobProxy proxy = new BrowserMobProxyServer();
-
         // Start proxy on a random available port
         proxy.start(0);
-
         // Enable HAR capture (headers, content, cookies, etc.)
         proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
-
         // Step 2: Start recording HAR
         proxy.newHar("exampleTest");
-
         // Step 3: Configure Selenium to use the proxy
         Proxy seleniumProxy = new Proxy();
         seleniumProxy.setHttpProxy("localhost:" + proxy.getPort());
-
         ChromeOptions options = new ChromeOptions();
         options.setCapability("proxy", seleniumProxy);
         options.addArguments("start-maximized");
-
         // Set WebDriver path and initialize it
         System.setProperty("webdriver.chrome.driver", "src/test/resource/driver/chromedriver.exe");
          driver = new ChromeDriver(options);
-
         // Step 4: Navigate to a website
         driver.get("https://demoqa.com");
-
          js= (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,300)");
-
-        try {
+        try
+        {
             HandlingForm();
         }
         catch(Exception e){
             System.out.println(e.getMessage());
         }
-
         // Step 5: Stop recording and save HAR file
         File harFile = new File("network_capture.har");
         proxy.getHar().writeTo(harFile);
-
         // Clean up resources
         driver.quit();
         proxy.stop();
@@ -72,8 +60,6 @@ public class SeleniumBrowserMobWithProxy {
         Forms_Frames.click();
         WebElement PracticeForm_span=driver.findElement(By.xpath( "//*[@class=\"accordion\"]//div[@class=\"element-group\"]//following::span[@class=\"text\" and contains(text(),\"Practice Form\")]"));
         PracticeForm_span.click();
-
-
         WebElement PracticeForm = driver.findElement(By.xpath("//*[@class=\"practice-form-wrapper\"]"));
         js= (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,350)", PracticeForm);
